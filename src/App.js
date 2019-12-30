cc9b42fee2ee6997610d7b601a181a82089f760a
Narek <nareksar077@gmail.com>
2019-12-30 18:58:17 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Navbar from './coponents/Navbar/Navbar'
+import TourList from './coponents/TourList/'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <TourList />
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/coponents/tourData.js
@@ -0,0 +1,29 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info: "Lorem Imsum 1"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info: "Lorem Imsum 2"
+  }, {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london palace tour",
+    info: "Lorem Imsum 3"
+  }, {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info: "Lorem Imsum 4"
+  }
+]
+
